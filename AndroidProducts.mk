d97e7d3f87d579ba26bf6d816306a95474916eca
J0SH1X <aljoshua.hell@gmail.com>
2024-09-22 20:10:47 +0200

    blazeify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dm1q.mk
+    $(LOCAL_DIR)/blaze_dm1q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dm1q-eng \
-    lineage_dm1q-user \
-    lineage_dm1q-userdebug
+    blaze_dm1q-eng \
+    blaze_dm1q-user \
+    blaze_dm1q-userdebug
--- blaze_dm1q.mk
+++ blaze_dm1q.mk
@@ -12,9 +12,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/dm1q/device.mk)
 
 # Inherit from the Lineage configuration.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+BLAZE_MAINTAINER := J0SH1X
+TARGET_GAPPS_ARCH := arm64
+WITH_GAPPS := true
 
-PRODUCT_NAME := lineage_dm1q
+PRODUCT_NAME := blaze_dm1q
 PRODUCT_DEVICE := dm1q
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := SM-S9110
